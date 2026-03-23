b6284b50b5f0e05df1762e1c2750eef21c9ea9e3
BillyLikesBacon <bongo.macgillicuddy@gmail.com>
2026-03-23 18:38:49 -0400

    sefesf

--- api/trigger.py
+++ api/trigger.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify, request
 import os
 import requests
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -16,21 +16,20 @@ def trigger():
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "Accept": "application/vnd.github+json"
     }
-    data = {"ref": BRANCH}
-
-    response = requests.post(url, json=data, headers=headers)
-    if response.status_code == 204:
+    r = requests.post(url, json={"ref": BRANCH}, headers=headers)
+    if r.status_code == 204:
         return jsonify({"status": "Workflow triggered!"})
     else:
-        return jsonify({"status": "Failed", "detail": response.json()}), 400
+        return jsonify({"status": "Failed", "detail": r.json()}), 400
 
-def main(vercel_request):
+def main(request):
+    # Vercel serverless entrypoint
     with app.test_request_context(
-        path=vercel_request.path,
-        base_url=vercel_request.base_url,
-        query_string=vercel_request.query_string,
-        method=vercel_request.method,
-        headers=vercel_request.headers,
-        data=vercel_request.get_data()
+        path=request.path,
+        base_url=request.base_url,
+        query_string=request.query_string,
+        method=request.method,
+        headers=request.headers,
+        data=request.get_data()
     ):
         return app.full_dispatch_request()
